5cf99fd5d67fb4a33534554c2671a996450c4133
William Lardner <william.lardner0109@gmail.com>
2021-06-21 22:28:39 +0100

    update 3

--- api/server.js
+++ api/server.js
@@ -1,17 +1,21 @@
 const util = require('minecraft-server-util');
-const apicache = require('apicache');
 
 module.exports = (req, res) => {
     const { ip, port } = req.query;
 
-    if (isNaN(port)) return res.json({error: "Port must be a number"});
-
-    await util.status(ip, {port: parseInt(port)})
-    .then((response) => {
-        return res.json({online: response.onlinePlayers});
-    })
-    .catch((err) => {
-        console.log(err);
-        return res.json({error: "Unknown error"});
+    res.json({
+        ip: ip,
+        port: port
     });
+
+    // if (isNaN(port)) return res.json({error: "Port must be a number"});
+
+    // await util.status(ip, {port: parseInt(port)})
+    // .then((response) => {
+    //     return res.json({online: response.onlinePlayers});
+    // })
+    // .catch((err) => {
+    //     console.log(err);
+    //     return res.json({error: "Unknown error"});
+    // });
 }

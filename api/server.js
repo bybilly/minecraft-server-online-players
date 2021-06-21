9622fb9c5576acdc4d9d2ea1c545f2eaaf23563c
William Lardner <william.lardner0109@gmail.com>
2021-06-21 22:52:34 +0100

    Fixed bug

--- api/server.js
+++ api/server.js
@@ -1,21 +1,16 @@
 const util = require('minecraft-server-util');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
+    console.log("API called");
     const { ip, port } = req.query;
 
-    res.json({
-        ip: ip,
-        port: port
+    if (isNaN(port)) return res.json({error: "Port must be a number"});
+
+    await util.status(ip, {port: parseInt(port)})
+    .then((response) => {
+        return res.json({online: response.onlinePlayers});
+    })
+    .catch((err) => {
+        return res.json({error: "Unknown error"});
     });
-
-    // if (isNaN(port)) return res.json({error: "Port must be a number"});
-
-    // await util.status(ip, {port: parseInt(port)})
-    // .then((response) => {
-    //     return res.json({online: response.onlinePlayers});
-    // })
-    // .catch((err) => {
-    //     console.log(err);
-    //     return res.json({error: "Unknown error"});
-    // });
 }

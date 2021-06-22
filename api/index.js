ab40d4dc1f32d385340b09f7b36e638e23528e21
William Lardner <william.lardner0109@gmail.com>
2021-06-22 12:29:52 +0100

    convert players online to string

--- api/index.js
+++ api/index.js
@@ -12,13 +12,17 @@ app.get('/api/players/:ip/:port', async (req, res) => {
 
     if (isNaN(port)) return res.json({error: "Port must be a number"});
 
-    await util.status(ip, {port: parseInt(port)})
-    .then((response) => {
-        return res.json({online: response.onlinePlayers});
-    })
-    .catch((err) => {
+    try {
+        await util.status(ip, {port: parseInt(port)})
+        .then((response) => {
+            return res.json({online: response.onlinePlayers.toString()});
+        })
+        .catch((err) => {
+            return res.json({error: "Unknown error"});
+        });
+    } catch (Exception) {
         return res.json({error: "Unknown error"});
-    });
+    }
 });
 
 module.exports = app;
